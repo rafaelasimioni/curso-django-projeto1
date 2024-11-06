8b1d1175c59832bba58bb2b167af3c0a2cd35d4b
Rafaela Simioni <rsimionideavila@gmail.com>
2024-11-05 21:17:46 -0300

    .

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 
 def home_view(request):
-    return render(request, 'recipes/pages/home.html',context={'name': 'Rafaela Simioni de Ávila'})
+    return render(request, 'recipes/pages/home.html',context={'recipes': [make_recipe() for _ in range(10)]})
 
 
 
 def recipe(request,id):
-    return render(request, 'recipes/pages/recipe-view.html',context={'name': 'Rafaela Simioni de Ávila'})
+    return render(request, 'recipes/pages/recipe-view.html',context={'recipe': make_recipe()})
